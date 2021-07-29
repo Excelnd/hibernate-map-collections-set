359ae4f71ac1207f03a296eb2e23577b43db0a96
Admin i <ihsanmpm@gmail.com>
2021-07-29 15:12:08 +0530

    Mapping sets

---
+++ src/main/java/com/ihs2code/hibernate/CreateStudentImagesSet.java
@@ -0,0 +1,57 @@
+package com.ihs2code.hibernate;
+
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ihs2code.hibernate.entity.Student;
+
+public class CreateStudentImagesSet {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create the object	
+			Student tempStudent  = new Student("Ihs", "Doe", "ihs@ihs2code.com");
+			Set<String> theImages = tempStudent.getImages();
+			
+			theImages.add("photo1.jpg"); // duplicate, filtered at HashSet
+			theImages.add("photo2.jpg");
+			theImages.add("photo3.jpg");
+			theImages.add("photo4.jpg");
+			theImages.add("photo4.jpg");
+			theImages.add("photo5.jpg");
+			theImages.add("photo5.jpg");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the object
+			System.out.println("Saving the student and Images... ");
+			session.persist(tempStudent);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done!!!");
+						
+		}
+		finally {
+			// clean up code
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
